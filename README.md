a11f3c3e5e471ab89cbf30fbfa3be50ac2434a1c
Michael Flynn <mflynn@cngann.com>
2016-08-27 08:32:13 -0700

    final polish

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mindbody-homework
 Homework for Mindbody
+
+Notes:
+	- The data for the sparklines was not present in the API information so I used a randomizer to generate the sparkline content.
+	- Due to this running ajax calls, please remember to run this on a server.
+	
--- script.js
+++ script.js
@@ -58,6 +58,16 @@ var barOptions = {
 		}]
 	}
 };
+/**
+ * Number.prototype.format(n, x)
+ *
+ * @param integer n: length of decimal
+ * @param integer x: length of sections
+ */
+Number.prototype.format = function(n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
 
 function get_random_sparkline(){
 	var r = [];
@@ -76,44 +86,91 @@ function get_random_barchart(){
 	return r;
 }
 
+var programs;
+var pricingOptions;
 
 $(function(){
-	$(".sales").each(function(){
-		var myBarChart = new Chart($(this), {
-		    type: 'bar',
-		    data: {
-				labels : ["Jan","Feb","Mar","Apr","May","Jun","Jul"],
-				datasets : [
-					{
-						backgroundColor : barColor1,
-						data : get_random_barchart()
-					},
-					{
-						backgroundColor : barColor2,
-						data : get_random_barchart()
-					}
-
-				]
-			},
-		    options: barOptions
+	$.getJSON("https://api.myjson.com/bins/5bdb3", function(data){
+		programs = data;
+		$.getJSON("https://api.myjson.com/bins/47axv", function(data){
+			pricingOptions = data;
+			buildPrograms();
 		});
 	});
 
+	function buildPrograms(){
+		console.log(programs);
+		console.log(pricingOptions);
+		var $mastertemplate = $('.programtemplate').detach();
+		// Build Programs
+		$.each(programs, function(k, program){
+			var $template = $mastertemplate.clone(true, true);
+			$template.removeClass('.programtemplate');
+			$('.programs').append($template);
 
-	$('.hardlinehere').each(function(){$(this).sparkline(get_random_sparkline(),hard_sparkline_line_options);});
-	$('.softlinehere').each(function(){$(this).sparkline(get_random_sparkline(),soft_sparkline_line_options);});
+			$template.find(".sales").each(function(){
+				var myBarChart = new Chart($(this), {
+					type: 'bar',
+					data: {
+						labels : ["Jan","Feb","Mar","Apr","May","Jun","Jul"],
+						datasets : [
+							{
+								backgroundColor : barColor1,
+								data : program.Sales.PreviousYear
+							},
+							{
+								backgroundColor : barColor2,
+								data : program.Sales.CurrentYear
+							}
 
-	$('.toggle').click(function(e){
-		e.preventDefault();
-		$(this).find('span').toggle();
-		$(this).parent().find('.more').toggle();
-	});
-	$('.menu a').click(function(e){
-		e.preventDefault();
-		$('.menu li').removeClass('active');
-		$(this).parent().addClass('active');
-		$(".submenu ul").hide();
-		$(".submenu ul[data-parentmenu='"+$(this).data('menu')+"']").show();
+						]
+					},
+					options: barOptions
+				});
+			});
 
-	})
+			$template.find('.hardlinehere').each(function(){$(this).sparkline(program.Sales.CurrentYear,hard_sparkline_line_options);});
+
+			$template.find('.totalsales').text('$'+program.TotalMonthlySales.format());
+
+			$template.find('.title').text(program.Name);
+
+			var $masterpricingtemplate = $template.find('.pricetemplate').detach();
+			// Build Programs
+			$.each(pricingOptions, function(k, price){
+				if(price.ProgramID == program.ProgramID){
+					var $pricetemplate = $masterpricingtemplate.clone(true, true);
+					$template.find('.prices').append($pricetemplate);
+					$pricetemplate.find('.price').text("$"+price.Sales.format());
+					$pricetemplate.find('.name').text(price.Name);
+				}
+			});
+
+			// place pricing options
+			$template.find('.softlinehere').each(function(){$(this).sparkline(get_random_sparkline(),soft_sparkline_line_options);});
+
+
+		});
+
+		dataLoaded();
+
+	}
+
+	function dataLoaded(){
+
+
+		$('.toggle').click(function(e){
+			e.preventDefault();
+			$(this).find('span').toggle();
+			$(this).parent().find('.more').toggle();
+		});
+		$('.menu a').click(function(e){
+			e.preventDefault();
+			$('.menu li').removeClass('active');
+			$(this).parent().addClass('active');
+			$(".submenu ul").hide();
+			$(".submenu ul[data-parentmenu='"+$(this).data('menu')+"']").show();
+
+		});
+	}
 });
